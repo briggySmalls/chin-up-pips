5bc00e7076e2ecfeb695b608cb0119d23c99a242
briggySmalls <briggySmalls90@gmail.com>
2020-02-13 00:48:50 +0000

    Implement an animation
    It is not clear to me why I can't get 'rotation' like effects, but
    whatever

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <div>
+    <div id="main">
       <h1>Chin up Pips!</h1>
-      <TextCarousel class="item" ref="activity-item" :options="activities" />
+      <TextCarousel id="activity-item" class="item" ref="activity-item" :options="activities" />
       <p>
         because
       </p>
-      <TextCarousel class="item" ref="reason-item" :options="reasons" />
+      <TextCarousel id="reason-item" class="item" ref="reason-item" :options="reasons" />
       <p>
         you got this x
       </p>
@@ -55,63 +55,56 @@ $foreground-color: #ccc;
 
 body {
   background-color: $background-color;
-}
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $foreground-color;
-
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-
-a {
-  color: $foreground-color;
-}
-
-#footer {
-  margin-top: 5em;
-}
-
-h1 {
-  font-size: 4em;
-}
-.item {
-  font-size: 2em;
-}
-#shuffle {
-  margin-top: 1em;
-}
-
-button {
-  border-radius: 5em;
-  border: 3px solid $foreground-color;
-  color: $foreground-color;
-  background-color: $background-color;
-  overflow: hidden;
-  padding: 1em 1em;
-  font-weight: bold;
-  font-size: 1.1em;
-  cursor: pointer;
-
-  &:hover {
-    background-color: lighten($background-color, 10%);
+  a {
+    color: $foreground-color;
   }
-}
 
-h1 {
-  font-size: 4em;
-}
-.item {
-  font-size: 2em;
-}
-#shuffle {
-  margin-top: 1em;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: $foreground-color;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  button {
+    border-radius: 5em;
+    border: 3px solid $foreground-color;
+    color: $foreground-color;
+    background-color: $background-color;
+    overflow: hidden;
+    padding: 1em 1em;
+    font-weight: bold;
+    font-size: 1.1em;
+    cursor: pointer;
+
+    &:hover {
+      background-color: lighten($background-color, 10%);
+    }
+  }
+
+  h1 {
+    font-size: 4em;
+  }
+  .item {
+    font-size: 2em;
+  }
+  #reason-item {
+    transition-delay: 0.1s;
+  }
+  #shuffle {
+    margin-top: 1em;
+  }
+
+  #footer {
+    margin-top: 5em;
+  }
 }
 </style>
--- src/components/TextCarousel.vue
+++ src/components/TextCarousel.vue
@@ -1,9 +1,7 @@
 <template>
-  <ul class="text-carousel">
-    <li class="option" v-for="option in options" :key="option">
-      {{ option }}
-    </li>
-  </ul>
+  <transition name="fade" v-on:after-leave="continueShuffle">
+    <div v-show="show" ref="selected">{{ selected }}</div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -15,24 +13,37 @@ export default class TextCarousel extends Vue {
 
   private index = 0;
 
+  private show = true;
+  private isShuffling = false;
+
   get selected() {
     return this.options[this.index];
   }
 
   shuffle() {
+    // Hide current
+    this.show = false;
+  }
+
+  continueShuffle(el) {
     // Get random integer
     this.index = Math.floor(Math.random() * this.options.length);
+    // Reveal again
+    this.show = true;
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.text-carousel {
-  .option {
-    position: absolute;
-    overflow: hidden;
-    width: 100%;
-  }
+.fade-enter-active, .fade-leave-active {
+  // transition: opacity .5s;
+  transition: all .3s ease;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-to {
+  transform: translateY(50%);
 }
 </style>
